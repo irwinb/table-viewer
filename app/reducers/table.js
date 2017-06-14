aea0b1892906b0cc055fe9cdcc0456c34aab59bd
Irwin Billing <irbillin@microsoft.com>
2017-06-14 09:42:35 -0700

    reordering

--- app/reducers/table.js
+++ app/reducers/table.js
@@ -9,13 +9,34 @@ type actionType = {
   type: string
 };
 
+/**
+ * Reducer that takes an action that contains:
+ * type      the action type.
+ * nextPage  the page to change to, if type is CHANGE_PAGE
+ */
+export function page(state: number = 1, action: actionType) {
+  switch (action.type) {
+    case CHANGE_PAGE:
+      return action.nextPage;
+    case INCREMENT_PAGE:
+      return state + 1;
+    case DECREMENT_PAGE:
+      return state <= 1 
+          ? state
+          : state - 1;
+    default return state;
+  }
+}
+
 export default function table(state: object = {}, action: actionType) {
   switch (action.type) {
-    case INCREMENT_COUNTER:
+    case CHANGE_PAGE:
       return state + 1;
-    case DECREMENT_COUNTER:
+    case INCREMENT_PAGE:
       return state - 1;
     default:
       return state;
+  }
+  return {
   }
 };
