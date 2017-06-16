7cae73c49676442c180e1bbaa03d58395b1a6301
Irwin Billing <irbillin@microsoft.com>
2017-06-16 14:38:29 -0700

    working more on reducer

--- app/reducers/table.js
+++ app/reducers/table.js
@@ -28,15 +28,29 @@ export function page(state: number = 1, action: actionType) {
   }
 }
 
+export function entries(state: array = [], action: actionType) {
+  switch (action.type) {
+    case CHANGE_PAGE:
+      
+      return action.nextPage;
+    default return state;
+  }
+}
+
 export default function table(state: object = {}, action: actionType) {
   switch (action.type) {
     case CHANGE_PAGE:
-      return state + 1;
     case INCREMENT_PAGE:
-      return state - 1;
+    case DECREMENT_PAGE:
+      state = {...,
+        page: page(state.page, action.type)
+      };
+      state.entries = entries(state.entries, {
+        type: CHANGE_PAGE,
+        page: state.page
+      });
+      return state;
     default:
       return state;
-  }
-  return {
   }
 };
